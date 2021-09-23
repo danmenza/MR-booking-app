5b210f25f7e860bd375b0a3cef02fc027dc481a2
danmenza <dan.menza@aeropay.com>
2021-09-22 21:28:45 -0500

    update to allow storage to s3 for artist_artwork and user images in separate buckets and display images

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,4 @@
 class Artist < ApplicationRecord
     has_many :reservations
-    has_many_attached :artist_artwork
+    has_many_attached :artist_artwork, service: :amazon_artists
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,5 +1,5 @@
 class Reservation < ApplicationRecord
     belongs_to :artist
     belongs_to :user
-    has_many_attached :artwork
+    has_many_attached :artwork, service: :amazon_users
 end
