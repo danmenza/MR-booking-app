c32ffcbac06720f42d7b2ce3353b084c69fe85b7
danmenza <dan.menza@aeropay.com>
2021-11-10 21:49:11 -0600

    updated verbiage for artist show and artist reservation request email. Added show user info page. Started initial work for app navigation page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,18 @@ class UsersController < ApplicationController
         @reservations = @user.reservations
     end
 
+    def show
+        authorize current_user
+        @user = current_user
+    end
+
     private
 
     def set_user
         @user = current_user
+    end
+
+    def reservation_params
+        params.require(:user).permit(:first_name, :last_name, :phone, :email)
     end
 end
