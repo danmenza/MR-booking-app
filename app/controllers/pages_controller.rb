a90f638260d67f4f5be96b1a1df7d2ca68daa812
danmenza <dan.menza@aeropay.com>
2021-12-04 12:43:15 -0600

    allow menu navigation if not logged in and fixed text styling for city filter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
-    skip_after_action :verify_authorized
+    include Pundit
+        skip_after_action :verify_authorized, only: [:navigation]
+        skip_before_action :authenticate_user!, only: [:navigation]
 
     def navigation
-        
     end
 end
