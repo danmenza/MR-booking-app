2123e70799c7eec73c48751e9a0c3aac60288a33
danmenza <dan.menza@aeropay.com>
2022-01-04 23:31:59 -0600

    added phone number validation for artist and user phone number. Updated primary buttons to be 100% width and container width to be 95% for additional padding on sides

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,42 @@ document.addEventListener('turbolinks:load', () => {
 
     $('#share_select').select2();
 });
+
+function phoneNumberFormatter() {
+    // grab the value of what the user is typing into the input
+    const inputField = document.querySelectorAll("artist_phone", "user_phone");
+
+    // next, we're going to format this input with the `formattPhoneNumber` function, which we'll write next.
+    const formattedInputValue = formatPhoneNumber(inputField.value);
+
+    // Then we'll set the value of the inputField to the formattedValue we generated with the formattPhoneNumber
+    inputField.value = formattedInputValue;
+}
+
+function formatPhoneNumber(value) {
+    // if input value is falsy eg if the user deletes the input, then just return
+    if (!value) return value;
+
+    // clean the input for any non-digit values.
+    const phoneNumber = value.replace(/[^\d]/g, "");
+
+    // phoneNumberLength is used to know when to apply our formatting for the phone number
+    const phoneNumberLength = phoneNumber.length;
+
+    // we need to return the value with no formatting if its less then four digits
+    // this is to avoid weird behavior that occurs if you  format the area code to early
+    if (phoneNumberLength < 4) return phoneNumber;
+
+    // if phoneNumberLength is greater than 4 and less the 7 we start to return
+    // the formatted number
+    if (phoneNumberLength < 7) {
+        return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+    }
+
+    // finally, if the phoneNumberLength is greater then seven, we add the last
+    // bit of formatting and return it.
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+      3,
+      6
+    )}-${phoneNumber.slice(6, 9)}`;
+}
