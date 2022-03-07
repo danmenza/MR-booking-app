b3809d789f5f1c06b0079fce254312bb94302435
danmenza <dan.menza@aeropay.com>
2022-03-06 22:38:03 -0700

    put CDN in front of artist, studio, and user images.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def cdn_for_artist(file)
+      "#{ENV['ARTIST_CDN_URL']}/#{file.key}"
+    end
+
+  def cdn_for_studio(file)
+    "#{ENV['STUDIO_CDN_URL']}/#{file.key}"
+  end
+
+  def cdn_for_user(file)
+    "#{ENV['USER_CDN_URL']}/#{file.key}"
+  end
 end
