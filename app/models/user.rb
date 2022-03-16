21660e56d92552a60ade9fff340bd1fd8e6f0e0f
danmenza <dan.menza@aeropay.com>
2022-03-15 22:57:29 -0600

    updated cloudfront urls for email templates. Fixed user delete if they had existing reservations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   validates :first_name, on: [:create, :update], format: { with: /\A[a-zA-Z ]+\z/, message:  "must only contain letters" }
   validates :last_name, on: [:create, :update], format: { with: /\A[a-zA-Z ]+\z/, message:  "must only contain letters" }
   validates :email, on: [:create, :update], uniqueness: true
