49b00c27d3439d90b7912cdc60dd06e87ba4cc9e
Dan Menza <danmenza@Dans-MBP.lan>
2022-05-15 16:05:36 -0600

    updated artist.studio_id if statements to resolve properly

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -6,7 +6,6 @@ class Artist < ApplicationRecord
     validates :artist_artwork, file_content_type: { allow: ['image/gif', 'image/png', 'image/jpg', 'image/jpeg'], message: 'must be file type gif, png, jpg, or jpeg' }, file_size: { less_than: 10.megabytes , message: 'must be less than 10MB in size' } 
     validates :name, format: { with: /\A[a-zA-Z ]+\z/, message:  "must only contain letters" } 
     validates :email, uniqueness: true, on: :create, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/, message:  "must contain valid format" }
-    validates :address, format: { with: /\A[a-zA-z0-9. \s]+\z/, message:  "must contain valid format" }
     validates :city, format: { with: /\A[a-zA-Z ]+\z/, message:  "must only contain letters" } 
     include PgSearch::Model
     pg_search_scope :search_by_city,
